d24bcd40bdb63a21b7d761e40167fe230a342184
raph.abdul <raph.abdul@pm.me>
2025-09-25 15:58:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// Must be set for req.ip to work correctly begind a proxy/load balancer
+// 'trust proxy' can be true or an array of IP addresses/subnets
+app.set('trust proxy', true); 
+
+app.get('/', (req, res)=>{
+	let clientIp = req.ip
+	let clientIpAlternatives = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : req.socket.remoteAddress;
+	res.status(200);
+	res.send("root URL of Server "+ clientIp +" also theres this: "+ clientIpAlternatives);
+});
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log ("Server running app listening to port "+ PORT);
+	else
+		console.log("Error occurrer", error);
+	}
+);
