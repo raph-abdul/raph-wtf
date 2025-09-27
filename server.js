c3cccb50b2476229e4784ec114e9d79b6db96138
raph.abdul <raph.abdul@pm.me>
2025-09-27 10:17:01 +0200

    .

--- server.js
+++ server.js
@@ -9,22 +9,14 @@ app.prepare().then(() => {
 
 	const server = express();
 
-  // Custom API routes
-  server.get('/old-page', (req, res) => {
-	let clientIp = req.ip
-	res.json({ message: "raph, wtf."+ clientIp });
-  });
+	server.all('/{*rest}', (req, res) => {
+		let clientIp = req.ip
+		res.json({ message: "raph, wtf."+ clientIp })
+	});
 
-  // Let Next.js handle everything else
-  server.all('/{*rest}', (req, res) => {
-	let clientIp = req.ip
-	res.json({ message: "raph, wtf."+ clientIp })
-//    return handle(req, res);
-  });
-
-  server.listen(3000, (err) => {
-	if (err) throw err;
-	console.log('> Ready on http://localhost:3000');
-  });
+	server.listen(3000, (err) => {
+		if (err) throw err;
+		console.log('> Ready on http://localhost:3000');
+	});
 });
 
